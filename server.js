d4678adc1d9a4e972ed7820835a516b466f7093b
Akos Toth-Mate <akos@tothmate.com>
2013-11-03 18:52:20 +0100

    p300 speller like color test

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = require('http').createServer(handler)
 app.listen(9111);
 
 function handler(req, res) {
-    fs.readFile(__dirname + '/index.html', function (err, data) {  
+    console.log(req)
+    fs.readFile(__dirname + req.url, function (err, data) {  
         res.writeHead(200);
         res.end(data);
     });
@@ -16,23 +17,26 @@ browser = null;
 io.set('log level', 1);
 io.sockets.on('connection', function (socket) { browser = socket });
 
-//setInterval(function() { if (browser) browser.emit('eeg', Math.floor((Math.random()*4095)-2048)); }, 50);
+TEST = true;
 
+if (TEST) {
+    setInterval(function() { if (browser) browser.emit('eeg', Math.floor((Math.random()*4095)-2048)); }, 50);
+} else {
+    thinkgear = net.connect(13854, '127.0.0.1', function() {
+        thinkgear.write(JSON.stringify({enableRawOutput: true, format: 'Json'}));
+    });
 
-thinkgear = net.connect(13854, '127.0.0.1', function() {
-    thinkgear.write(JSON.stringify({enableRawOutput: true, format: 'Json'}));
-});
-
-buffer = "";
-thinkgear.on('data', function(data) {
-    buffer += data.toString();
-    var packets = buffer.split('\r');
-    if (packets.length == 0) return;
-    buffer = packets.pop();
-    for (var i in packets) {
-        var packet = JSON.parse(packets[i]);
-        if (packet.hasOwnProperty('rawEeg') && browser) {
-            browser.emit('eeg', packet['rawEeg']);
+    buffer = "";
+    thinkgear.on('data', function(data) {
+        buffer += data.toString();
+        var packets = buffer.split('\r');
+        if (packets.length == 0) return;
+        buffer = packets.pop();
+        for (var i in packets) {
+            var packet = JSON.parse(packets[i]);
+            if (packet.hasOwnProperty('rawEeg') && browser) {
+                browser.emit('eeg', packet['rawEeg']);
+            }
         }
-    }
-});
+    });
+}
